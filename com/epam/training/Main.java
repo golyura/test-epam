8c43f33638c6b0f103605b27e2d5937720627531
Golubev Yury <gol.y@mail.ru>
2023-07-11 12:02:49 +0300

    Final bugfix

--- com/epam/training/Main.java
+++ com/epam/training/Main.java
@@ -5,7 +5,7 @@ public class Main  {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-                System.out.println("Hay world");
+                System.out.println("Bug fix22");
 	}
 
 
