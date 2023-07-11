e8f350da3eb73e0803fd2c415679e876f5c4fba1
Golubev Yury <gol.y@mail.ru>
2023-07-11 18:08:38 +0300

    Add readme

--- com/epam/training/Main.java
+++ com/epam/training/Main.java
@@ -6,6 +6,7 @@ public class Main  {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
                 System.out.println("Bug fix22");
+				Cat cat = new Cat();
 	}
 
 
